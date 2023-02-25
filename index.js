180eeb302e283c1f3ccdad69cb8952566f6af2f7
skylersaucedo <skyler.saucedo@gmail.com>
2023-02-25 09:48:08 -0600

    added nlp and console response

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-const {dockStart} = require('@nlpjs/basic'); // create artifacts and containers
+const {dock, dockStart} = require('@nlpjs/basic'); // create artifacts and containers
+const {NlpAnalyzer} = require('@nlpjs/utils'); // import analyzer utils
+const corpus = require('./data/corpus-test-en.json');
+
+// looking at training..
+
+// (async() => {
+//     const dock = await dockStart();
+//     const nlp = dock.get('nlp');
+//     await nlp.train();
+
+//     const result = await nlp.process('When is your birthday?');
+//     console.log(result);
+//     console.log(result.answer);
+// })();
+
+async function trainFn(corpus){
+    const nlp = dock.get('nlp');
+    nlp.addCorpus(corpus);
+    await nlp.train()
+    return nlp
+
+}
+
+function testFn(utterance, nlp){
+
+    return nlp.process(utterance);
+
+}
+
+// use this to generate a report on training/testing nlp model as .xlsx
+
+// (async() => {
+//     await dockStart();
+//     const analyzer = new NlpAnalyzer();
+//     const analysis = await analyzer.analyze(corpus, trainFn, testFn);
+//     await analyzer.generateExcel('./measure.xlsx', analysis);
+//     console.log(analysis)
+// })();
+
+
+// use this to converse with your bot in the console
 
 (async() => {
-    const dock = await dockStart();
+    await dockStart();
     const nlp = dock.get('nlp');
     await nlp.train();
-
-    const result = await nlp.process('When is your birthday?');
-    console.log(result);
-    console.log(result.answer);
-
 })();
